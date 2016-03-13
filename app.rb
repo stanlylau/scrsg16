482609701469082c2730528e21a63e7316f4535f
Stanly Lau <stanly@odd-e.com>
2016-03-13 16:52:11 +0800

    Updated fb social picture

--- app.rb
+++ app.rb
@@ -43,7 +43,7 @@ before '/:locale/*' do
       @twitter_card.card_type = "summary"
       @twitter_card.site = "@stanlylau"
       @twitter_card.creator = "@stanlylau"
-      @twitter_card.image_url = "http://scrsg16.herokuapp.com/assets/img/retreat/social.jpg"
+      @twitter_card.image_url = "http://scrsg16.herokuapp.com/assets/img/retreat/fb_social.jpg"
 
       @facebook_og = FacebookOpenGraph.new
       @facebook_og.site_name = ""
